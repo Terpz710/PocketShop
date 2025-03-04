b0cd9a194c1ab8fe7e95432b2dbcbfe4b5d5e917
WavyCraftNetwork <142659461+Terpz710@users.noreply.github.com>
2025-03-03 23:52:55 -0800

    Update PocketShop.php

--- src/terpz710/pocketshop/PocketShop.php
+++ src/terpz710/pocketshop/PocketShop.php
@@ -8,15 +8,11 @@ use pocketmine\plugin\PluginBase;
 
 use terpz710\pocketshop\command\ShopCommand;
 
-use terpz710\pocketshop\form\ShopForm;
-
 use CortexPE\Commando\PacketHooker;
 
 final class PocketShop extends PluginBase {
 
     protected static self $instance;
-
-    private ShopForm $shop;
 
     protected function onLoad() : void{
         self::$instance = $this;
@@ -30,15 +26,9 @@ final class PocketShop extends PluginBase {
         }
 
         $this->getServer()->getCommandMap()->register("PocketShop", new ShopCommand($this, "shop", "opens a shop menu"));
-
-        $this->shop = new ShopForm($this);
     }
 
     public static function getInstance() : self{
         return self::$instance;
-    }
-
-    public function getShopForm() : ShopForm{
-        return $this->shop;
     }
 }
